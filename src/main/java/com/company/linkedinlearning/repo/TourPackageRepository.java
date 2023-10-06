cb305bc05df870875946ecf152db70155f924343
Rashid Alakbarov <resid328@gmail.com>
2023-10-06 15:09:37 +0400

    Update TourPackageRepository.java

--- src/main/java/com/company/linkedinlearning/repo/TourPackageRepository.java
+++ src/main/java/com/company/linkedinlearning/repo/TourPackageRepository.java
@@ -2,14 +2,43 @@ package com.company.linkedinlearning.repo;
 
 import com.company.linkedinlearning.domain.TourPackage;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.Optional;
 
 /**
  * Tour Package Repository Interface
  */
+
+@RepositoryRestResource(collectionResourceRel = "package",path = "package")
 public interface TourPackageRepository extends CrudRepository<TourPackage, String> {
 
-    Optional<TourPackage> findByName(String name);
+    Optional<TourPackage> findByName(@Param("name") String name);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends TourPackage> S save(S entity);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends TourPackage> Iterable<S> saveAll(Iterable<S> entities);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(String s);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(TourPackage entity);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends TourPackage> entities);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
 }
 
